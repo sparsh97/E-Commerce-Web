af541ccd12694e1a17e9ce6cad933d63e4272660
sparsh97 <sparshvermadmc@gmail.com>
2022-07-04 18:22:21 +0530

    cross-orgin support added

--- backend/spring-boot-ecommerce/src/main/java/com/ecommerce/dao/ProductCategoryRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/ecommerce/dao/ProductCategoryRepository.java
@@ -4,7 +4,9 @@ import com.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
--- backend/spring-boot-ecommerce/src/main/java/com/ecommerce/dao/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/ecommerce/dao/ProductRepository.java
@@ -2,7 +2,9 @@ package com.ecommerce.dao;
 
 import com.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
