0588535ea2b321665ad650e9ab05629b0ac9b745
sparsh97 <sparshvermadmc@gmail.com>
2022-07-05 21:41:10 +0530

    image added and bootstrap and fotaesome with routing added

--- frontend/angular-ecommerce-web-app/src/app/commom/product.ts
+++ frontend/angular-ecommerce-web-app/src/app/commom/product.ts
@@ -6,7 +6,7 @@ export class Product {
     unitPrice!: number;
     imageUrl!: string;
     active!: boolean;
-    unitInStock!: number;
+    unitsInStock!: number;
     dateCreated!: Date;
     lastUpdated!: Date;
 
